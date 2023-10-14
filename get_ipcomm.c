9de87af894652f00d6ef913c9994d5412dcc9c08
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-14 11:27:10 -0700

    Updating main + Adding get ipcomm

---
+++ get_ipcomm.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_ipcomm - Function for reading input command
+ *
+ *Description : Fnuction for reading the input command from user
+ *
+ *Return: pointer to the input string, Null if failed
+ */
+
+char *get_ipcomm(void)
+{
+	char *inbuffer = NULL;
+	size_t inbufsize = 0;
+	ssize_t	chars;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+	chars = getline(&inbuffer, &inbufsize, stdin);
+	if (chars == -1)
+	{
+		free(inbuffer);
+		return (NULL);
+	}
+	return (inbuffer);
+}
